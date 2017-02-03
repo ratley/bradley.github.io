63c9891ac9e0627e8c7b252289d09bb828ae02a4
bradley inniss <bfinniss@gmail.com>
2017-02-03 06:03:38 -0800

    under construction projects view

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
+//   $(window).scroll(function (event) {
+//     var scroll = $(window).scrollTop();
+//     console.log(scroll)
+// });
   $('#about-link a').on('click', function() {
     // $('#projects-link a').removeClass('active')
     // $('#projectsBorderBottom').css('width', '0px')
     // $('#about-link a').addClass('active')
     // $('#aboutBorderBottom').css('width', '38px')
     $('html, body').animate({
-      scrollTop: $("#about").offset().top
+      scrollTop: $("#about").offset().top - 80
     }, 1000);
   })
 
@@ -20,18 +24,18 @@ $(document).ready(function() {
     }, 1000);
   })
 {/* <a href="http://github.com/ratley" target="_blank">github</a> */}
-  $('#projects-link').hover(function() {
-    $(this).html('under construction')
-    $(this).css('transition', 'color .5s ease')
-    $(this).css('color', 'gold')
-  })
-  $(this).css('color', 'gold')
-  $(document).on('hover','#projects-link a', function() {
-  })
-  $('#projects-link').on('mouseleave', function() {
-    $(this).text('projects')
-    $(this).css('color', '#616161')
-  })
+  // $('#projects-link').hover(function() {
+  //   $(this).html('under construction')
+  //   $(this).css('transition', 'color .5s ease')
+  //   $(this).css('color', 'gold')
+  // })
+  // $(this).css('color', 'gold')
+  // $(document).on('hover','#projects-link a', function() {
+  // })
+  // $('#projects-link').on('mouseleave', function() {
+  //   $(this).text('projects')
+  //   $(this).css('color', '#616161')
+  // })
 
 
   checkLocation()
@@ -60,7 +64,7 @@ function checkScrollPosition() {
     $('#projectsBorderBottom').css('width', '0px')
     $('#about-link a').addClass('active')
     $('#aboutBorderBottom').css('width', '38px')
-  } else if($(document).scrollTop() > 300) {
+  } else if($(document).scrollTop() > 750) {
     $('#about-link a').removeClass('active')
     $('#aboutBorderBottom').css('width', '0px')
     $('#projects-link a').addClass('active')
